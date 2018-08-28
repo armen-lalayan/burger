6e619101a7f74531b0b2467ed3806d9b04fac130
armen lalayan <armen.lalaian.88@gmail.com>
2018-08-28 15:56:47 +0200

    week_5-2

--- app/js/module.js
+++ app/js/module.js
@@ -193,6 +193,24 @@ for(let i = 0; i < reviewsButtons.length; i++){
 
 //  ------------------------------  map
 
+ymaps.ready(init);
+      
+        function init(){ 
+            var myMap = new ymaps.Map("map", {
+                center: [55.76, 37.64],
+                zoom: 7
+            }); 
+            
+            var myPlacemark = new ymaps.Placemark([55.76, 37.64], {
+                hintContent: 'Содержимое всплывающей подсказки',
+                balloonContent: 'Содержимое балуна'
+            });
+            
+            myMap.geoObjects.add(myPlacemark);
+        }
+
+
+
 // function initMap(){
 //     var element = document.getElementById('map');
 //     var  options = {
